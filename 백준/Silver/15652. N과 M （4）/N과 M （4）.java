fb47689870f90f03c9e7eb77fc89b1499081720e
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-02-20 12:54:29 +0900

    [Silver III] Title: N과 M (4), Time: 288 ms, Memory: 22304 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,37 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class Main {
+	static int[] arr;
+	static BufferedWriter bw;
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt(); // 1부터 N까지 자연수
+		int M = sc.nextInt(); // 수열의 길이 M
+
+		arr = new int[M];
+		bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		dfs(N, M, 0, 1);
+		bw.flush();
+	}
+
+	public static void dfs(int N, int M, int depth, int index) throws IOException {
+		if (depth == M) {
+			for (int v : arr) {
+				bw.write(v + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		for (int i = 1; i <= N; i++) {
+			arr[depth] = i;
+			if (i >= index) {
+				dfs(N, M, depth + 1, index);
+				index++;
+			}
+		}
+	}
+}
