2a7fc658c00725a465c92f154257ec241f08f6b2
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-02-27 22:10:36 +0900

    [Silver III] Title: 수열, Time: 316 ms, Memory: 26820 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.stream.Stream;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] nk = Stream.of(br.readLine().split(" "))
+				.mapToInt(Integer::parseInt).toArray();
+		int[] temperature = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+		int[] prefixSum = new int[nk[0] + 1];
+
+		// 온도 자료구조와 구간합 자료 만들기
+		for (int i = 1; i <= nk[0]; i++) {
+			prefixSum[i] = prefixSum[i - 1] + temperature[i - 1];
+		}
+
+		// 연속적인 K일의 온도의 합이 최대가 되는 값 출력
+		// 투포인터를 적용해서 K일에 해당하지 않는 구간합을 빼준 sum을 구한다.
+		int maxSum = Integer.MIN_VALUE;
+		for (int i = nk[1]; i <= nk[0]; i++) {
+			int sum = prefixSum[i] - prefixSum[i - nk[1]];
+			maxSum = Math.max(maxSum, sum);
+		}
+		System.out.println(maxSum);
+	}
+
+}
