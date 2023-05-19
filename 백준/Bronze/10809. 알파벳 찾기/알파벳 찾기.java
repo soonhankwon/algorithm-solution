c31d05111797be5cc7b9b8fb0be9abc87b17e59b
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-05-19 22:01:11 +0900

    Update 알파벳 찾기.java

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,31 +1,10 @@
-import java.io.IOException;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
-public class Main {
-	public static void main(String args[]) throws IOException {
-		Scanner scan = new Scanner(System.in);
-		String s = scan.next();
-		char[] words = new char[s.length()];
-		
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		char[] alphabetList = new char[alphabet.length()];
-		
-		int[] answerSheet = new int[alphabet.length()];
-		
-		for(int i=0; i<alphabet.length(); i++) {
-			alphabetList[i] = alphabet.charAt(i);
-			answerSheet[i] = -1;
-			
-			for(int j=0; j<s.length(); j++) {
-				words[j] = s.charAt(j);
-				if(alphabetList[i] == words[j]) {
-					answerSheet[i] = j;
-					break;
-				} 
-			}
-		}
-		for(int i: answerSheet) {
-			System.out.print(i + " ");
-		}
-	} 
+public class P10809_알파벳 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        IntStream.range(97, 123).forEach(i -> System.out.print(str.indexOf(i) + " "));
+    }
 }
