ac6e9fab602b5bf541853fe357aac3299e036e47
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-15 16:36:03 +0900

    Update ［1차］ 캐시.java

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -1,8 +1,8 @@
 import java.util.LinkedList;
 import java.util.Queue;
 
-class Solution {
-    public int solution(int cacheSize, String[] cities) {
+public class 캐시 {
+    public static int solution(int cacheSize, String[] cities) {
         Queue<String> cacheQueue = new LinkedList<>();
         int answer = 0;
         if(cacheSize == 0) {
@@ -10,10 +10,9 @@ class Solution {
         }
         for (String city : cities) {
             String cityLowerCased = city.toLowerCase();
-            if (cacheQueue.size() < cacheSize) {
+            if (cacheQueue.size() <= cacheSize) {
                 if(isCityExistCache(cacheQueue, cityLowerCased)) {
-                    cacheQueue.remove(cityLowerCased);
-                    cacheQueue.add(cityLowerCased);
+                    cachingCaseByHit(cacheQueue, cityLowerCased);
                     answer++;
                 }
                 else {
@@ -23,28 +22,32 @@ class Solution {
             }
             else {
                 if(isCityExistCache(cacheQueue, cityLowerCased)) {
-                    cacheQueue.remove(cityLowerCased);
-                    cacheQueue.add(cityLowerCased);
+                    cachingCaseByHit(cacheQueue, cityLowerCased);
                     answer++;
                 }
                 else {
-                    cacheData(cacheQueue, cityLowerCased);
+                    cachingCaseByMiss(cacheQueue, cityLowerCased);
                     answer = cacheMiss(answer);
                 }
             }
         }
         return answer;
     }
-    
+
+    private static void cachingCaseByHit(Queue<String> cacheQueue, String cityLowerCased) {
+        cacheQueue.remove(cityLowerCased);
+        cacheQueue.add(cityLowerCased);
+    }
+
     private static boolean isCityExistCache(Queue<String> cacheQueue, String city) {
         return cacheQueue.contains(city);
     }
 
-    private static void cacheData(Queue<String> cacheQueue, String city) {
+    private static void cachingCaseByMiss(Queue<String> cacheQueue, String city) {
         cacheQueue.poll();
         cacheQueue.add(city);
     }
-    
+
     private static int cacheMiss(int answer) {
         answer += 5;
         return answer;
