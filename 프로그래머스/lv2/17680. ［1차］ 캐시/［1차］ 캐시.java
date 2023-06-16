8c58aa059d1a9490c8cec6629197e86765db87a0
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-16 10:29:56 +0900

    [level 2] Title: [1차] 캐시, Time: 2.81 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -2,54 +2,62 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
+    
+    private static int answer;
+    
     public int solution(int cacheSize, String[] cities) {
         Queue<String> cacheQueue = new LinkedList<>();
-        int answer = 0;
-        if(cacheSize == 0) {
+        answer = 0;
+        if (isNotUsedCache(cacheSize)) {
             return cities.length * 5;
         }
-        for (String city : cities) {
-            String cityLowerCased = city.toLowerCase();
-            if (cacheQueue.size() < cacheSize) {
-                if(isCityExistCache(cacheQueue, cityLowerCased)) {
-                    cachingCaseByHit(cacheQueue, cityLowerCased);
-                    answer++;
-                }
-                else {
-                    cacheQueue.add(cityLowerCased);
-                    answer = cacheMiss(answer);
-                }
+
+        for (String c : cities) {
+            String city = c.toLowerCase();
+            if (isCacheSpaceUnderCacheSize(cacheSize, cacheQueue)) {
+                getExecutionTime(cacheSize, cacheQueue, city);
             }
             else {
-                if(isCityExistCache(cacheQueue, cityLowerCased)) {
-                    cachingCaseByHit(cacheQueue, cityLowerCased);
-                    answer++;
-                }
-                else {
-                    cachingCaseByMiss(cacheQueue, cityLowerCased);
-                    answer = cacheMiss(answer);
-                }
+                getExecutionTime(cacheSize, cacheQueue, city);
             }
         }
         return answer;
     }
     
-    private static void cachingCaseByHit(Queue<String> cacheQueue, String cityLowerCased) {
-        cacheQueue.remove(cityLowerCased);
-        cacheQueue.add(cityLowerCased);
+    private static boolean isNotUsedCache(int cacheSize) {
+        return cacheSize == 0;
+    }
+
+    private static boolean isCacheSpaceUnderCacheSize(int cacheSize, Queue<String> cacheQueue) {
+        return cacheQueue.size() < cacheSize;
+    }
+
+    private static void getExecutionTime(int cacheSize, Queue<String> cacheQueue, String city) {
+        if (isCityExistCache(cacheQueue, city)) {
+            cachingCaseByHit(cacheQueue, city);
+            answer++;
+        } else {
+            cachingCaseByMissAndCacheSpace(cacheQueue, city, isCacheSpaceUnderCacheSize(cacheSize, cacheQueue));
+            answer += 5;
+        }
     }
 
     private static boolean isCityExistCache(Queue<String> cacheQueue, String city) {
         return cacheQueue.contains(city);
     }
 
-    private static void cachingCaseByMiss(Queue<String> cacheQueue, String city) {
-        cacheQueue.poll();
-        cacheQueue.add(city);
+    private static void cachingCaseByHit(Queue<String> cacheQueue, String cityLowerCased) {
+        cacheQueue.remove(cityLowerCased);
+        cacheQueue.add(cityLowerCased);
     }
 
-    private static int cacheMiss(int answer) {
-        answer += 5;
-        return answer;
+    private static void cachingCaseByMissAndCacheSpace(Queue<String> cacheQueue, String city, boolean isCacheSpaceUnderCacheSize) {
+        if (isCacheSpaceUnderCacheSize) {
+            cacheQueue.add(city);
+        }
+        else {
+            cacheQueue.poll();
+            cacheQueue.add(city);
+        }
     }
 }
