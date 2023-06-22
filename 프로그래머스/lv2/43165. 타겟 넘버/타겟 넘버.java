f7986e20e5622a7e9ba24682704f36b67be6e2dd
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-22 09:48:53 +0900

    [level 2] Title: 타겟 넘버, Time: 0.69 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    static int count;
+    static int targetNumber;
+    
+    public int solution(int[] numbers, int target) {
+        count = 0;
+        targetNumber = target;
+        dfs(numbers[0], numbers, 1);
+        dfs(-numbers[0], numbers, 1);
+        return count;
+    }
+    
+    private static void dfs(int sum, int[] numbers, int depth) {
+        if (isDepthFloor(numbers, depth)) {
+            if (isSumTargetNumber(sum, targetNumber)) {
+                count++;
+            }
+            return;
+        }
+        dfs(sum + numbers[depth], numbers, depth + 1);
+        dfs(sum - numbers[depth], numbers, depth + 1);
+    }
+
+    private static boolean isDepthFloor(int[] numbers, int depth) {
+        return isSumTargetNumber(depth, numbers.length);
+    }
+
+    private static boolean isSumTargetNumber(int sum, int targetNumber) {
+        return sum == targetNumber;
+    }
+}
