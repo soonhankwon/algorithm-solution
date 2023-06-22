bab2859bbae4efbbefe7544d9d2e5f3185e745b2
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-22 10:35:49 +0900

    refactor 타겟 넘버.java
    exception

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     
     static int count;
@@ -6,18 +8,19 @@ class Solution {
     public int solution(int[] numbers, int target) {
         count = 0;
         targetNumber = target;
+        Arrays.sort(numbers);
+        if (isNotTargetNumber(numbers)) {
+            return count;
+        }
         dfs(numbers[0], numbers, 1);
         dfs(-numbers[0], numbers, 1);
+
         return count;
     }
     
     private static void dfs(int sum, int[] numbers, int depth) {
-        if (isNotTargetNumber(numbers)) {
-            return;
-        }
-        
         if (isDepthFloor(numbers, depth)) {
-            if (isSumTargetNumber(sum, targetNumber)) {
+            if (isSumTargetNumber(sum)) {
                 count++;
             }
             return;
@@ -25,7 +28,7 @@ class Solution {
         dfs(sum + numbers[depth], numbers, depth + 1);
         dfs(sum - numbers[depth], numbers, depth + 1);
     }
-    
+
     private static boolean isNotTargetNumber(int[] numbers) {
         int first = numbers[0];
         int last = numbers[numbers.length - 1];
@@ -36,7 +39,7 @@ class Solution {
         return depth == numbers.length;
     }
 
-    private static boolean isSumTargetNumber(int sum, int targetNumber) {
+    private static boolean isSumTargetNumber(int sum) {
         return sum == targetNumber;
     }
 }
