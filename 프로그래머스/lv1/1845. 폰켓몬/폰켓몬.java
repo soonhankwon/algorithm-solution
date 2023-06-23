c00221a025aae0ba2a6902e3f0c49edf861cb99b
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-23 10:13:08 +0900

    Update 폰켓몬.java
    V2, V3

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -9,4 +9,17 @@ class Solution {
 
         return Math.min(pickChance, removedDuplicatesPocketMonNum);
     }
+
+    // Set
+    private static int solutionV2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.stream(nums).forEach(set::add);
+
+        return set.size() > nums.length / 2 ? nums.length / 2 : set.size();
+    }
+
+    // Short Coding
+    private static int solutionV3(int[] nums) {
+        return Math.min(nums.length / 2, Arrays.stream(nums).distinct().toArray().length);
+    }
 }
