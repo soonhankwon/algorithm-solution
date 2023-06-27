0dd8890ef5c7fa6047a8b6520e6fcd4a88ee492d
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-27 11:38:12 +0900

    [level 2] Title: 의상, Time: 1.43 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        Arrays.stream(clothes)
+                .forEach(cloth -> map.put(cloth[1], map.getOrDefault(cloth[1], 0) + 1));
+
+        int answer = map.values().stream()
+                .mapToInt(value -> value + 1)
+                .reduce(1, (a, b) -> a * b);
+
+        return answer - 1;
+    }
+}
