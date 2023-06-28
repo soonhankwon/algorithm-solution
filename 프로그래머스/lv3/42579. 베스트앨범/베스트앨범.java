616e068b9d2b0e9936eaf50a2d715e47214a93f7
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-28 10:43:34 +0900

    Update 베스트앨범.java
    change line

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.java
@@ -1,14 +1,21 @@
 import java.util.*;
 
-class Solution {
-    public int[] solution(String[] genres, int[] plays) {
+public class 베스트앨범 {
+    public static void main(String[] args) {
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+
+        System.out.println(Arrays.toString(solution(genres, plays)));
+    }
+
+    private static int[] solution(String[] genres, int[] plays) {
         Map<String, Integer> genreCountMap = new HashMap<>();
         List<Song> songs = new ArrayList<>();
         for (int i = 0; i < genres.length; i++) {
             genreCountMap.put(genres[i], genreCountMap.getOrDefault(genres[i], 0) + plays[i]);
             songs.add(new Song(i, genres[i], plays[i]));
         }
-        
+
         songs.sort(Comparator.comparing((Song s) -> genreCountMap.get(s.getGenre()))
                 .thenComparing(Song::getGenre)
                 .thenComparingInt(Song::getPlayCount)
@@ -16,13 +23,13 @@ class Solution {
 
         List<Integer> result = new ArrayList<>();
         int tempCount = 0;
-        String tempGenre = songs.get(0).getGenre();
-        for(Song song : songs) {
-            if(!song.getGenre().equals(tempGenre)) {
-                tempGenre = song.getGenre();
+        String curGenre = songs.get(0).getGenre();
+        for (Song song : songs) {
+            if (isSongEqualCurGenre(curGenre, song)) {
+                curGenre = song.getGenre();
                 tempCount = 0;
             }
-            if(tempCount == 2) {
+            if (tempCount == 2) {
                 continue;
             }
             result.add(song.getId());
@@ -32,13 +39,18 @@ class Solution {
         return result.stream().mapToInt(Integer::intValue)
                 .toArray();
     }
-    
-    private static class Song{
-        private int id;
 
-        private String genre;
+    private static boolean isSongEqualCurGenre(String curGenre, Song song) {
+        return !song.getGenre().equals(curGenre);
+    }
 
-        private int playCount;
+    private static class Song {
+        
+        private final int id;
+
+        private final String genre;
+
+        private final int playCount;
 
         public Song(int id, String genre, int playCount) {
             this.id = id;
