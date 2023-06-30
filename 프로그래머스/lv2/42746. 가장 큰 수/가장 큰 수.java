e2ccb171a56f1f192a01657e4feac4a3685ab0b7
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-06-30 11:22:03 +0900

    Update 가장 큰 수.java
    refactor

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -3,21 +3,14 @@ import java.util.Arrays;
 class Solution {
     
     public String solution(int[] numbers) {
-        String[] numbersArr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            numbersArr[i] = String.valueOf(numbers[i]);
-        }
+        String[] numbersArr = Arrays.stream(numbers)
+                .mapToObj(String::valueOf)
+                .sorted((a, b) -> (b + a).compareTo(a + b))
+                .toArray(String[]::new);
 
-        Arrays.sort(numbersArr, (a, b) -> (b + a).compareTo(a + b));
         if(numbersArr[0].equals("0")) {
             return "0";
         }
-
-        StringBuilder sb = new StringBuilder();
-        for (String number : numbersArr) {
-            sb.append(number);
-        }
-
-        return sb.toString();
+        return String.join("", numbersArr);
     }
 }
