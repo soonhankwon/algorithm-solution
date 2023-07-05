a660233eafe6ba1101f422860cc7f6a2400fd718
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-07-05 09:09:36 +0900

    [level 2] Title: 올바른 괄호, Time: 5.43 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        char[] chars = s.toCharArray();
+        if (chars[0] == ')' || chars[chars.length - 1] != ')') {
+            return false;
+        }
+
+        int count = 0;
+        for (char aChar : chars) {
+            if (aChar == '(') {
+                count++;
+            } else {
+                count--;
+                if(count < 0) {
+                    return false;
+                }
+            }
+                
+        }
+
+        return count == 0;
+    }
+}
