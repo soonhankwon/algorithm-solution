c6a572b84f35c1083420306e553d3f675c44ba32
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-07-06 10:12:49 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 5.12 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,55 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        Queue<Truck> truckQueue = Arrays.stream(truck_weights)
+                .mapToObj(Truck::new)
+                .collect(Collectors.toCollection(LinkedList::new));
+
+        int curBridgeWeight = weight;
+        Queue<Truck> bridgeQueue = new LinkedList<>();
+        int turn = 1;
+        while(!truckQueue.isEmpty() || !bridgeQueue.isEmpty()) {
+            if(truckQueue.peek() != null) {
+                if(curBridgeWeight >= truckQueue.peek().getWeight()) {
+                    Truck firstTruck = truckQueue.poll();
+                    bridgeQueue.add(firstTruck);
+                    curBridgeWeight -= firstTruck.getWeight();
+                }
+            }
+            bridgeQueue.iterator().forEachRemaining(Truck::move);
+            if(bridgeQueue.stream().anyMatch(i -> i.getPosition() > bridge_length)) {
+                Truck passedTruck = bridgeQueue.poll();
+                curBridgeWeight += passedTruck.getWeight();
+            }
+            turn++;
+        }
+
+        return turn;
+    }
+    
+    private static class Truck {
+        private final int weight;
+        private int position;
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public int getPosition() {
+            return position;
+        }
+
+        public void move() {
+            this.position++;
+        }
+
+        public Truck(int weight) {
+            this.weight = weight;
+            this.position = 1;
+        }
+    }
+}
