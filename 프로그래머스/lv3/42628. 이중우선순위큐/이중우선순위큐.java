4fafb8647cfb41e3766a57f1cd953305e9884162
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-07-07 11:24:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 3.04 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(String[] operations) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (String operation : operations) {
+            String[] split = operation.split(" ");
+            if(split[0].equals("I")) {
+                numbers.add(Integer.valueOf(split[1]));
+            } else if (split[0].equals("D")) {
+                if(split[1].equals("-1")) {
+                    if(numbers.isEmpty()) {
+                        continue;
+                    }
+                    Integer min = numbers.stream().mapToInt(Integer::intValue).min().orElseThrow();
+                    numbers.remove(min);
+                } else {
+                    if(numbers.isEmpty()) {
+                        continue;
+                    }
+                    Integer max = numbers.stream().mapToInt(Integer::intValue).max().orElseThrow();
+                    numbers.remove(max);
+                }
+            }
+        }
+
+        if(numbers.isEmpty()) {
+            return new int[]{0,0};
+        } else {
+            int max = numbers.stream().mapToInt(Integer::intValue).max().orElseThrow();
+            int min = numbers.stream().mapToInt(Integer::intValue).min().orElseThrow();
+
+            return new int[] {max, min};
+        }
+    }
+}
