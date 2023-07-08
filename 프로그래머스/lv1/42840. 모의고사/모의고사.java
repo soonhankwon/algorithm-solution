aef6ebb81a650cc55124962849baea69b9c1849f
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-07-09 06:08:15 +0900

    [level 1] Title: 모의고사, Time: 3.24 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] c = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+
+        int count = count(answers, a);
+        int count2 = count(answers, b);
+        int count3 = count(answers, c);
+
+        int[] res = {count, count2, count3};
+
+        int max = Arrays.stream(res).max().orElse(0);
+        ArrayList<Integer> ans = new ArrayList<>();
+        int i = 1;
+        for (int r : res) {
+            if(r == max)
+                ans.add(i);
+            i++;
+        }
+
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static int count(int[] answer, int[] myAnswer) {
+        int count = 0;
+        int i = 0;
+        for (int a : answer) {
+            if (i >= myAnswer.length)
+                i = 0;
+            if (a == myAnswer[i]) {
+                count++;
+            }
+            i++;
+        }
+        return count;
+    }
+}
