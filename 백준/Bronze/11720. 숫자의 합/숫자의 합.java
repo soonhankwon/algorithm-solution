567ef51f67c78dc57f38b71fe8254f10471d8e4c
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-08-10 19:28:04 +0900

    Update 숫자의 합.java

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		br.readLine();
-		
-		int sum = 0;
-		
-		for(byte value : br.readLine().getBytes()) {
-			sum+= (value - '0');
-		}
-		System.out.println(sum);
-	} 
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        br.readLine();
+        String[] split = br.readLine().split("");
+        int sum = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
+        System.out.println(sum);
+    }
 }
