0f7d88239c038522e75d873275fef58aaba31d35
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-08-22 18:20:37 +0900

    Update 수열.java
    index 수정

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,31 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int[] nk = Stream.of(br.readLine().split(" "))
-				.mapToInt(Integer::parseInt).toArray();
-		int[] temperature = Stream.of(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-		int[] prefixSum = new int[nk[0] + 1];
+        int n = inputs[0];
+        int k = inputs[1];
 
-		// 온도 자료구조와 구간합 자료 만들기
-		for (int i = 1; i <= nk[0]; i++) {
-			prefixSum[i] = prefixSum[i - 1] + temperature[i - 1];
-		}
+        int[] records = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
-		// 연속적인 K일의 온도의 합이 최대가 되는 값 출력
-		// 투포인터를 적용해서 K일에 해당하지 않는 구간합을 빼준 sum을 구한다.
-		int maxSum = Integer.MIN_VALUE;
-		for (int i = nk[1]; i <= nk[0]; i++) {
-			int sum = prefixSum[i] - prefixSum[i - nk[1]];
-			maxSum = Math.max(maxSum, sum);
-		}
-		System.out.println(maxSum);
-	}
+        int[] prefixSum = new int[n + 1];
+        IntStream.rangeClosed(1, n)
+                .forEach(i -> prefixSum[i] = prefixSum[i - 1] + records[i - 1]);
 
+        int maxDegree = Integer.MIN_VALUE;
+        for (int i = k; i <= n; i++) {
+            int sum = prefixSum[i] - prefixSum[i - k];
+            maxDegree = Math.max(maxDegree, sum);
+        }
+        System.out.println(maxDegree);
+    }
 }
