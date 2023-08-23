f105732171205083d42ee4ee828e8dc1b39aab90
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-08-23 18:08:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 2724 ms, Memory: 203340 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputs = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int n = inputs[0];
+        int m = inputs[1];
+
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            int[] data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            for (int j = 1; j <= n; j++) {
+                map[i][j] = data[j - 1];
+            }
+        }
+
+        int[][] prefixSum = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                prefixSum[i][j] =
+                        prefixSum[i][j - 1] + prefixSum[i - 1][j] - prefixSum[i - 1][j - 1] + map[i][j];
+            }
+        }
+
+        for (int i = 1; i <= m; i++) {
+            int[] data = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int x1 = data[0];
+            int y1 = data[1];
+            int x2 = data[2];
+            int y2 = data[3];
+            int result = prefixSum[x2][y2] - prefixSum[x1 - 1][y2] - prefixSum[x2][y1 - 1] + prefixSum[x1 - 1][y1 - 1];
+            System.out.println(result);
+        }
+    }
+}
