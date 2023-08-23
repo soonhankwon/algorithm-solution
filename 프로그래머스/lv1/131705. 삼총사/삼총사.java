3ee69fa0cd5299c66a6ada97530d5dedf08308d7
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-08-23 11:52:02 +0900

    [level 1] Title: 삼총사, Time: 0.05 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -2,16 +2,11 @@ class Solution {
     public int solution(int[] number) {
         int answer = 0;
         int length = number.length;
-        
         for (int i = 0; i < length; i++) {
             for (int j = i + 1; j < length; j++) {
                 for (int k = j + 1; k < length; k++) {
-                    if(i == j || j == k || k == i) {
-                        continue;
-                    }
-                    if (number[i] + number[j] + number[k] == 0) {
+                    if (number[i] + number[j] + number[k] == 0)
                         answer++;
-                    }
                 }
             }
         }
