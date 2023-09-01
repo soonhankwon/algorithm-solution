abf52b9ded5b8f989e5819998349c122d702ab2d
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2023-09-01 18:20:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 3.21 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1)
+            return new int[]{-1};
+        int min = Arrays.stream(arr).min().orElseThrow();
+        return Arrays.stream(arr).filter(i -> i != min).toArray();
+    }
+}
