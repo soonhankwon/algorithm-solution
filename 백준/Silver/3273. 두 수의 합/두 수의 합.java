30dfaf4feb3992cdf2d6582d3072fa4c526b3f89
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-02-05 14:54:54 +0900

    [Silver III] Title: 두 수의 합, Time: 420 ms, Memory: 35448 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,35 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] arr = new int[n];
-		for (int i = 0; i < n; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
-		// 오름차 순으로 정렬 
-		Arrays.sort(arr);
-		int cnt = 0;
-		int x = sc.nextInt();
-		// two pointer 알고리즘 사용 
-		int left = 0, right = n - 1;
-		while(left < right) {
-			int sum = arr[left] + arr[right];
-			if(sum < x) {
-				left++;
-			} else if (sum > x) {
-				right--;
-			} else {
-				cnt++;
-				left++;
-				right--;
-			}
-		}
-		System.out.println(cnt);
-	}
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .sorted()
+                .toArray();
+
+        // eg) 1,2,3,5,7,9,10,11,12
+
+        int x = Integer.parseInt(br.readLine());
+        // two-pointer
+        int index1 = 0;
+        int index2 = n - 1;
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            if (index1 >= index2) {
+                break;
+            }
+            int sum = inputs[index1] + inputs[index2];
+            if (sum > x) {
+                index2--;
+                continue;
+            }
+            if (sum < x) {
+                index1++;
+                continue;
+            }
+            answer++;
+            index1++;
+            index2--;
+        }
+
+        System.out.println(answer);
+    }
 }
