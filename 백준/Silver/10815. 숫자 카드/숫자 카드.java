549d93abec13920cbad2b660d5de8f3881dec853
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-02-06 12:13:25 +0900

    [Silver V] Title: 숫자 카드, Time: 908 ms, Memory: 141544 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			int x = Integer.parseInt(st.nextToken());
+			map.put(x, map.getOrDefault(x, 0) + 1);
+		}
+		
+		int m = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+		
+		for (int i = 0; i < m; i++) {
+			int y = Integer.parseInt(st.nextToken());
+			if (map.get(y) == null) {
+				bw.write(0 + "\n");
+			} else {
+				bw.write(1 + "\n");
+			}
+		}
+		bw.flush();
+		bw.close();
+	}
+
+}
