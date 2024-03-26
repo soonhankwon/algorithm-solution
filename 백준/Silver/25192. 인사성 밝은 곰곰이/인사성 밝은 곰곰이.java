1bb3b0ec3b9ea2d5ef680b8e01b47786b426b622
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-03-26 16:23:31 +0900

    [Silver IV] Title: 인사성 밝은 곰곰이, Time: 284 ms, Memory: 25528 KB -BaekjoonHub

---
+++ 백준/Silver/25192. 인사성 밝은 곰곰이/인사성 밝은 곰곰이.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine()); //9
+        Set<String> set = new HashSet<>();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            if (input.equals("ENTER")) {
+                cnt += set.size();
+                set.clear();
+                continue;
+            }
+            set.add(input);
+        }
+        cnt += set.size();
+        System.out.println(cnt);
+        br.close();
+    }
+}
