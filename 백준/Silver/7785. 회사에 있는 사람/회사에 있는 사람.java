f60096d1bb80beb0e4c93d418121649a95e42aa9
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-03-27 22:46:59 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 1680 ms, Memory: 57660 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -1,17 +1,25 @@
-import java.util.*;
-import java.util.stream.IntStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-
-        Map<String, String> map = new HashMap<>();
-        IntStream.range(0, n).forEach(i -> map.put(scanner.next(), scanner.next()));
-
-        new ArrayList<>(map.keySet()).stream()
-                .filter(i -> map.get(i).equals("enter"))
-                .sorted(Comparator.reverseOrder())
-                .forEach(System.out::println);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<String> set = new TreeSet<>(Comparator.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            String[] inputs = br.readLine().split(" ");
+            String teamMate = inputs[0];
+            if (set.contains(teamMate)) {
+                set.remove(teamMate);
+                continue;
+            }
+            set.add(inputs[0]);
+        }
+        set.forEach(System.out::println);
+        br.close();
     }
 }
