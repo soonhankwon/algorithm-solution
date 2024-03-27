7bc5dda60569d86a4d12d142a2416189461d8295
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-03-27 21:28:01 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // StringBuilder reverse
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder(br.readLine());
+        String reversedStr = sb.reverse().toString();
+
+        String[] inputs = reversedStr.split(" ");
+        int a = Integer.parseInt(inputs[0]);
+        int b = Integer.parseInt(inputs[1]);
+
+        if (a > b) {
+            System.out.println(a);
+            return;
+        }
+        System.out.println(b);
+    }
+}
