57c66a5911f44185885657b12317954dab735ee0
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-04-28 23:58:03 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -4,16 +4,20 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static final int MOD = 1234567891;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String input = br.readLine();
 
         long answer = 0;
+        long pow = 1;
         for (int i = 0; i < n; i++) {
-            answer += (long) ((input.charAt(i) - 96) * Math.pow(31, i));
+            answer += (input.charAt(i) - 96) * pow;
+            pow = (pow * 31) % MOD;
         }
-        System.out.println(answer);
+        System.out.println(answer % MOD);
         br.close();
     }
 }
