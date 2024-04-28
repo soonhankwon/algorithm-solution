dae8c8bb63a8f7997d17d44911440cdd8b2677d7
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-04-28 11:40:11 +0900

    [Bronze I] Title: 팰린드롬수, Time: 112 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            sb.append(isPalindrome(input) ? "yes\n" : "no\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+
+    private static boolean isPalindrome(String word) {
+        int start = 0;
+        int end = word.length() - 1;
+        while (start < end) {
+            if (word.charAt(start) != word.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
