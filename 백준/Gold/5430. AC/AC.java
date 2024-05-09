abf2959330213867b96d54a29ee7c1af009573ac
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-09 12:06:33 +0900

    [Gold V] Title: AC, Time: 736 ms, Memory: 93588 KB -BaekjoonHub

---
+++ 백준/Gold/5430. AC/AC.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        int tc = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < tc; i++) {
+            boolean isError = false;
+            String operation = br.readLine();
+            br.readLine();
+            String input = br.readLine();
+            char[] chars = operation.toCharArray();
+
+            Deque<Integer> deque = new ArrayDeque<>();
+            int inputLength = input.length();
+            if (inputLength > 2) {
+                String[] split = input.substring(1, inputLength - 1).split(",");
+                for (String s : split) {
+                    deque.addLast(Integer.parseInt(s));
+                }
+            }
+
+            boolean isReversed = false;
+            for (char ch : chars) {
+                if (ch == 'R') {
+                    isReversed = !isReversed;
+                    continue;
+                }
+                if (ch == 'D') {
+                    if (deque.isEmpty()) {
+                        isError = true;
+                        break;
+                    }
+                    if (isReversed) {
+                        deque.pollLast();
+                        continue;
+                    }
+                    deque.pollFirst();
+                } else {
+                    isError = true;
+                    break;
+                }
+            }
+            if (isError) {
+                sb.append("error\n");
+            } else {
+                print(deque, isReversed);
+            }
+            //케이스 끝
+        }
+        System.out.println(sb);
+    }
+
+    static void print(Deque<Integer> deque, boolean isReverse) {
+
+        if (deque.isEmpty()) {
+            sb.append("[]\n");
+            return;
+        }
+        sb.append("[");
+        int size = deque.size();
+        if (isReverse) {
+            for (int i = 0; i < size - 1; i++) {
+                sb.append(deque.pollLast()).append(",");
+            }
+        } else {
+            for (int i = 0; i < size - 1; i++) {
+                sb.append(deque.pollFirst()).append(",");
+            }
+        }
+        sb.append(deque.poll()).append("]\n");
+    }
+}
