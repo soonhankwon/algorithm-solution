3733014bb679fe9ead57c8cba7337b4f33bb2464
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-27 18:20:37 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st  = new StringTokenizer(br.readLine()," ");
-		
-		System.out.println(st.countTokens());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        long count = Arrays.stream(str.strip().split(" "))
+                .filter(s -> !s.isEmpty())
+                .count();
+        System.out.println(count);
+        br.close();
+    }
 }
