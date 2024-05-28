59b4fc19b6793377bb5caaf38db434aaed95ee13
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-29 08:35:42 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 140 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -1,14 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        // 7개의 숫자중 3개를 뽑는 조합의 수 nCr
-        // 7! / (7-3)! * 3!
-        // 7 * 5 / 1 = 35
-        long cnt = ((long) n * (n - 1) * (n - 2)) / 6;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // 7개 숫자중 3개를 뽑는 조합 -> 7! / (7-3)! * 3!
+        // 7 * 6 * 5 / 3 * 2
+        long cnt = ((long) (n - 2) * (n - 1) * n) / 6;
         System.out.println(cnt + "\n" + 3);
+        br.close();
     }
 }
