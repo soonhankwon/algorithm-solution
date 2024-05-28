c79fd827a6f1110530ae5e4c599ab2efee7faeef
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-28 21:45:03 +0900

    [Silver IV] Title: 듣보잡, Time: 440 ms, Memory: 29292 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,31 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
+import java.util.Arrays;
 import java.util.TreeMap;
 
 public class Main {
-    //듣보잡 -> 두케이스중 중복되는 이름을 찾아라 & 사전순
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
+        int n = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .sum();
 
-        int cnt = 0;
-        Map<String, Integer> map = new TreeMap<>();
-        for (int i = 0; i < n + m; i++) {
-            String key = br.readLine();
-            map.put(key, map.getOrDefault(key, 0) + 1);
-            if (map.get(key) == 2) {
-                cnt++;
-            }
+        TreeMap<String, Integer> map = new TreeMap<>();
+        for (int i = 0; i < n; i++) {
+            map.compute(br.readLine(), (k, v) -> v == null ? 1 : ++v);
         }
+        StringBuilder sb = new StringBuilder();
+        long count = map.entrySet().stream()
+                .filter(e -> e.getValue() == 2)
+                .count();
+        sb.append(count).append("\n");
 
-        System.out.println(cnt);
-        map.keySet().stream()
-                .filter(k -> map.get(k) == 2)
-                .forEach(System.out::println);
+        map.entrySet().stream()
+                .filter(e -> e.getValue() == 2)
+                .forEach(e -> sb.append(e.getKey()).append("\n"));
+        System.out.println(sb);
         br.close();
     }
 }
