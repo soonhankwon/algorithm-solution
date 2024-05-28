46643ac3295cc5834be0f17ed0713ae20cda3b20
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-28 14:54:59 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 684 ms, Memory: 53724 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -2,24 +2,24 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Set<String> set = new TreeSet<>(Comparator.reverseOrder());
+        Map<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
         for (int i = 0; i < n; i++) {
-            String[] inputs = br.readLine().split(" ");
-            String teamMate = inputs[0];
-            if (set.contains(teamMate)) {
-                set.remove(teamMate);
-                continue;
-            }
-            set.add(inputs[0]);
+            map.compute(br.readLine().split(" ")[0],
+                    (k, v) -> v == null ? 1 : ++v);
         }
-        set.forEach(System.out::println);
+        StringBuilder sb = new StringBuilder();
+        map.entrySet()
+                .stream()
+                .filter(e -> e.getValue() % 2 == 1)
+                .forEach(e -> sb.append(e.getKey()).append("\n"));
+        System.out.println(sb);
         br.close();
     }
 }
