d4b682f172739fcc6b9a22d921ab1839813f13b3
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-01 00:00:17 +0900

    [Silver I] Title: 추월, Time: 136 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2002. 추월/추월.java
+++ 백준/Silver/2002. 추월/추월.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        // 들어갔을때 나의 앞차량보다 빨리나오면 추월한것
-        Queue<String> queue = new LinkedList<>();
+        Queue<String> queue = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             queue.add(br.readLine());
         }
-
         int cnt = 0;
         for (int i = 0; i < n; i++) {
-            String result = br.readLine();
-            if (!result.equals(queue.peek())) {
-                queue.remove(result);
+            String res = br.readLine();
+            if (!res.equals(queue.peek())) {
+                queue.remove(res);
                 cnt++;
                 continue;
             }
