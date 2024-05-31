c23eac158a24fe41181548fd12989f78624e04ae
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-05-31 11:48:23 +0900

    [Silver I] Title: 마니또, Time: 128 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Silver/5107. 마니또/마니또.java
+++ 백준/Silver/5107. 마니또/마니또.java
@@ -3,20 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
 
     static Map<String, String> relationMap;
+    static int n, cnt;
+    static String[] userIndexArr = new String[20];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= n; i++) {
             // 관계 맵
             relationMap = new HashMap<>();
             // 친구 최대 20명
-            String[] userIndexArr = new String[20];
             for (int j = 0; j < n; j++) {
                 String[] row = br.readLine().split(" ");
                 String user1 = row[0];
@@ -25,13 +27,13 @@ public class Main {
                 relationMap.put(user1, user2);
             }
 
-            int cnt = 0;
+            cnt = 0;
             String end;
             for (String start : userIndexArr) {
                 end = relationMap.get(start);
                 while (true) {
                     end = relationMap.get(end);
-                    if (end == null) {
+                    if (Objects.isNull(end)) {
                         break;
                     } else if (end.equals(start)) {
                         cnt++;
