273edd94fb893ce21dd175ecd52d7aea20bf0026
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-05 19:06:52 +0900

    Update 소수 단어.java

--- 백준/Bronze/2153. 소수 단어/소수 단어.java
+++ 백준/Bronze/2153. 소수 단어/소수 단어.java
@@ -1,24 +1,25 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Function<Integer, Integer> hashFunction = c -> {
+            if (c >= 97 && c <= 122) {
+                return c - 96;
+            }
+            return c - 38;
+        };
         int sum = br.readLine().chars()
-                .map(Main::hashFunction)
+                .map(hashFunction::apply)
                 .sum();
         System.out.println(isPrimeNumber(sum) ? "It is a prime word." : "It is not a prime word.");
         br.close();
     }
 
-    private static char hashFunction(int c) {
-        if (c >= 97 && c <= 122) {
-            return (char) (c - 96);
-        }
-        return (char) (c - 38);
-    }
-
+    // 에라스토텔레스 체
     private static boolean isPrimeNumber(int num) {
         if (num == 2 || num == 3) {
             return true;
@@ -26,7 +27,7 @@ public class Main {
         if (num % 2 == 0 || num % 3 == 0) {
             return false;
         }
-        for (int i = 5; Math.pow(i, 2) <= num; i += 6) {
+        for (int i = 5; i <= Math.sqrt(num); i++) {
             if (num % i == 0 || num % (i + 2) == 0) {
                 return false;
             }
