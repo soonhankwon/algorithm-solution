ed15f3d06cd682619f015a0691f3a899266df686
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-05 21:26:24 +0900

    [Silver I] Title: 절댓값 힙, Time: 380 ms, Memory: 26012 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -6,31 +6,25 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        // 절대값 힙!
-        // 1. 배열에 정수 x를 넣음
-        // 2. 배열에서 절대값이 가장 작은 값을 출력, 그 값을 배열에서 제거
-        // 2.1 가장 작은 값이 여러개이면, 가장 작은수를 출력!
-        // 3. x가 0이 아니라면 배열에 추가하는 연산
-        // 4. x가 0이 라면 절댓값이 가장 작은 값을 출력하고 배열에서 제거하는 연산
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PriorityQueue<Integer> queue = new PriorityQueue<>(
-                Comparator.comparingInt((Integer i) -> Math.abs(i))
-                        .thenComparing(i -> i)
-        );
         int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingInt((Integer i) -> Math.abs(i))
+                .thenComparingInt(i -> i));
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            int input = Integer.parseInt(br.readLine());
-            if (input != 0) {
-                queue.add(input);
+            int operation = Integer.parseInt(br.readLine());
+            if (operation != 0) {
+                pq.add(operation);
                 continue;
             }
-            Integer poll = queue.poll();
-            if (poll == null) {
-                System.out.println(0);
-                continue;
+            if (!pq.isEmpty()) {
+                sb.append(pq.poll());
+            } else {
+                sb.append(0);
             }
-            System.out.println(poll);
+            sb.append("\n");
         }
+        System.out.println(sb);
         br.close();
     }
 }
