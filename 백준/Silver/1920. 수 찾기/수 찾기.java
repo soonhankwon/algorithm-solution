9aadce433c2f3324badb0efbab7ebcce27003d4a
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-05 22:40:48 +0900

    [Silver IV] Title: 수 찾기, Time: 604 ms, Memory: 55096 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,23 +2,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         br.readLine();
-        int[] arr = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .sorted()
-                .toArray();
+        Set<Integer> set = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::valueOf)
+                .collect(Collectors.toSet());
         br.readLine();
         StringBuilder sb = new StringBuilder();
-        int[] row = Arrays.stream(br.readLine().split(" "))
+        Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
-                .toArray();
-        Arrays.stream(row)
-                .map(num -> Arrays.binarySearch(arr, num) < 0 ? 0 : 1)
-                .forEach(res -> sb.append(res).append("\n"));
+                .forEach(i -> {
+                    sb.append(set.contains(i) ? 1 : 0).append("\n");
+                });
         System.out.println(sb);
         br.close();
     }
