1d291b4ec30d423858a8c8344e95d2f414b33cb7
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-07 03:55:22 +0900

    [Silver II] Title: 나무 자르기, Time: 1108 ms, Memory: 142076 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,40 +2,36 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int n = inputs[0];
+        int m = inputs[1];
         int[] trees = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::valueOf)
+                .mapToInt(Integer::parseInt)
                 .sorted()
                 .toArray();
 
-        // 10 15 17 20
-        long start = 0;
-        long end = trees[n - 1];
-        // 이분탐색 활용
-        while (start <= end) {
-            long mid = (start + end) / 2;
+        int left = 0;
+        int right = trees[n - 1];
+        while (left <= right) {
+            int mid = (left + right) / 2;
             long sum = Arrays.stream(trees)
                     .filter(i -> i > mid)
                     .mapToLong(i -> i - mid)
                     .sum();
-            // 자른 나무 합계가 요구치 이상인 경우 start 포인터 up
             if (sum >= m) {
-                start = mid + 1;
-            }
-            // 자른 나무 합계가 요구치 미만인 경우 end 포인터 down
-            else {
-                end = mid - 1;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        System.out.println(end);
+        System.out.println(right);
+        br.close();
     }
 }
