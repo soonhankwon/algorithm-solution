4243432e48b9186edc8e1f0aaafaf662157f78ea
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-06 21:44:57 +0900

    [Bronze I] Title: 콘서트, Time: 656 ms, Memory: 134672 KB -BaekjoonHub

--- 백준/Bronze/16466. 콘서트/콘서트.java
+++ 백준/Bronze/16466. 콘서트/콘서트.java
@@ -2,22 +2,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        br.readLine();
-        Set<Integer> set = Arrays.stream(br.readLine().split(" "))
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = Arrays.stream(br.readLine().split(" "))
                 .map(Integer::valueOf)
-                .collect(Collectors.toSet());
-        for (int i = 1; i <= 1_000_000; i++) {
-            if (!set.contains(i)) {
-                System.out.println(i);
-                break;
+                .collect(Collectors.toCollection(PriorityQueue::new));
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!pq.isEmpty()) {
+                int min = pq.poll();
+                if (i != min) {
+                    answer = i;
+                    break;
+                }
             }
         }
+        System.out.println(answer == 0 ? n + 1 : answer);
         br.close();
     }
 }
