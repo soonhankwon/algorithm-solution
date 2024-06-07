55ffb7fa55e9e691a5473c257e62b2d0fa11cc34
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-07 19:55:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -2,41 +2,47 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
-
-    static List<Integer> arr;
+    static List<Integer> list, result;
+    static StringBuilder sb;
+    static boolean isFound;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int sum = 0;
-        arr = new ArrayList<>();
+        list = new ArrayList<>();
+        result = new ArrayList<>();
         for (int i = 0; i < 9; i++) {
-            int num = Integer.parseInt(br.readLine());
-            sum += num;
-            arr.add(num);
+            list.add(Integer.parseInt(br.readLine()));
         }
-        findTwoDwarfAndDelete(sum);
-        Collections.sort(arr);
-        StringBuilder sb = new StringBuilder();
-        arr.forEach(i -> sb.append(i).append("\n"));
+        list.sort(Integer::compareTo);
+        sb = new StringBuilder();
+        recursion(0, 0, 0);
         System.out.println(sb);
         br.close();
     }
 
-    private static void findTwoDwarfAndDelete(int sum) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = i + 1; j < 9; j++) {
-                Integer dwarf1 = arr.get(i);
-                Integer dwarf2 = arr.get(j);
-                if (sum - dwarf1 - dwarf2 == 100) {
-                    arr.remove(dwarf1);
-                    arr.remove(dwarf2);
-                    return;
-                }
+
+    static void recursion(int index, int count, int sum) {
+        if (isFound) {
+            return;
+        }
+        if (count == 7) {
+            if (sum == 100) {
+                result.forEach(i -> sb.append(i).append("\n"));
+                isFound = true;
+                return;
             }
+            return;
+        }
+
+        for (int i = index; i < 9; i++) {
+            sum += list.get(i);
+            result.add(list.get(i));
+            recursion(i + 1, count + 1, sum);
+            sum -= list.get(i);
+            result.remove(list.get(i));
         }
     }
 }
