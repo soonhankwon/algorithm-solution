8666c643cb0075a5eaa9d38444a2004ee7dfffb3
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-06-08 15:27:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1196 ms, Memory: 150460 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -5,66 +5,54 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
-/*
- * DFS
- */
 public class Main {
 
-    static boolean[] visited;
-    static int[] memo;
     static ArrayList<ArrayList<Integer>> graph;
-    static int n, m, r, cnt;
+    static boolean[] visited;
+    static int[] result;
+    static int num;
 
     public static void main(String[] args) throws IOException {
-        // n 개의 정점, m 개의 간선 그리고 무방향 그래프이다.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] inputs = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        n = inputs[0]; //vertex 5
-        m = inputs[1]; //edge 5
-        r = inputs[2]; //start 1
-
-        // 간선이 1부터 시작하므로 인덱스 때문에 0~n 까지 추가
+        int n = inputs[0];
+        int m = inputs[1];
+        int r = inputs[2];
         graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
+        for (int i = 0; i < n + 1; i++) {
             graph.add(new ArrayList<>());
         }
-
         for (int i = 0; i < m; i++) {
             int[] row = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-
             int v1 = row[0];
             int v2 = row[1];
             graph.get(v1).add(v2);
             graph.get(v2).add(v1);
         }
+        graph.forEach(Collections::sort);
 
-        for (ArrayList<Integer> g : graph) {
-            Collections.sort(g);
-        }
-
-        cnt = 1;
-        memo = new int[n];
         visited = new boolean[n + 1];
+        result = new int[n];
+        num = 1;
         dfs(r);
-        for (int m : memo) {
-            System.out.println(m);
-        }
+        StringBuilder sb = new StringBuilder();
+        Arrays.stream(result).forEach(i -> sb.append(i).append("\n"));
+        System.out.println(sb);
         br.close();
     }
 
-    private static void dfs(int vertex) {
-        visited[vertex] = true;
-        memo[vertex - 1] = cnt;
-
-        ArrayList<Integer> edges = graph.get(vertex);
-        for (int nextVertex : edges) {
-            if (!visited[nextVertex]) {
-                cnt++;
-                dfs(nextVertex);
+    private static void dfs(int node) {
+        visited[node] = true;
+        result[node - 1] = num;
+        ArrayList<Integer> adjNodes = graph.get(node);
+        for (Integer adjNode : adjNodes) {
+            if (!visited[adjNode]) {
+                num++;
+                dfs(adjNode);
             }
         }
     }
