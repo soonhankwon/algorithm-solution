66b0cda971b6d1c779fce82046b4686ae823f6db
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-16 21:53:16 +0900

    [Bronze I] Title: 2007년, Time: 128 ms, Memory: 14580 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] data = Arrays.stream(br.readLine().split(" "))
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-
-        int month = data[0];
-        int day = data[1];
-        DayOfWeek dayOfWeek = LocalDate.of(2007, month, day)
-                .getDayOfWeek();
-        System.out.println(dayOfWeek.toString().substring(0, 3));
+        System.out.println(
+                LocalDate.of(2007, inputs[0], inputs[1])
+                        .getDayOfWeek()
+                        .toString()
+                        .substring(0, 3)
+        );
         br.close();
     }
 }
