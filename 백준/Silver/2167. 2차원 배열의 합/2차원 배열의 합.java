f130f70d077c15a7ee98a246e27881deea19000f
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-18 08:54:50 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 408 ms, Memory: 32456 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -12,22 +12,18 @@ public class Main {
 
         int n = inputs[0];
         int m = inputs[1];
-        int[][] arr = new int[n][m];
         int[][] prefixSum = new int[n + 1][m + 1];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Arrays.stream(br.readLine().split(" "))
+        // 배열값 + 왼쪽 누적합 + 위쪽 누적합 - 교집합 누적합
+        for (int i = 1; i <= n; i++) {
+            int[] row = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-
-        }
-
-        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
                 prefixSum[i][j] =
-                        prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + arr[i - 1][j - 1];
+                        prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + row[j - 1];
             }
         }
-        
+
         StringBuilder sb = new StringBuilder();
         int k = Integer.parseInt(br.readLine());
         for (int i = 0; i < k; i++) {
@@ -35,6 +31,7 @@ public class Main {
                     .mapToInt(Integer::parseInt)
                     .toArray();
             int i0 = row[0], j0 = row[1], x0 = row[2], y0 = row[3];
+            // 전체 누적합 - 왼쪽 누적합 - 위쪽 누적합 + 교집합 누적합
             sb.append(prefixSum[x0][y0] - prefixSum[i0 - 1][y0] - prefixSum[x0][j0 - 1] + prefixSum[i0 - 1][j0 - 1])
                     .append("\n");
         }
