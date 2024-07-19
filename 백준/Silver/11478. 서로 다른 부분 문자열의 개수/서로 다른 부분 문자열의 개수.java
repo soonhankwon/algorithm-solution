5fadcec8b6b0476f6f34c8f3a92b995d4f912b01
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-19 22:26:21 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 656 ms, Memory: 223092 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -5,28 +5,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
+
+    static String str;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String input = br.readLine();
-
-        Set<String> set = new HashSet<>();
-        int length = input.length();
-        int startIndex = 0;
-        int endIndex = 0;
-        int windowSize = 1;
-        for (int i = 0; i < length + 1; i++) {
-            for (int j = 0; j < length + 1; j++) {
-                if (endIndex + windowSize > length) {
-                    //index 초기화
-                    startIndex = 0;
-                    endIndex = 0;
-                    windowSize++;
-                    break;
-                }
-                String str = input.substring(startIndex, endIndex + windowSize);
-                set.add(str);
-                startIndex++;
-                endIndex++;
+        str = br.readLine();
+        int length = str.length();
+        // 부분 문자열의 개수 계산
+        int substrings = length * (length + 1) / 2;
+        // 로드 팩터를 고려하여 약간 더 크게 설정
+        int capacity = (int) (substrings / 0.75) + 1;
+        // 다음 2의 거듭제곱으로 반올림
+        capacity--;
+        capacity |= capacity >>> 1;
+        capacity |= capacity >>> 2;
+        capacity |= capacity >>> 4;
+        capacity |= capacity >>> 8;
+        capacity |= capacity >>> 16;
+        capacity++;
+        Set<String> set = new HashSet<>(capacity);
+        for (int i = 0; i < length; i++) {
+            for (int j = i + 1; j < length + 1; j++) {
+                set.add(str.substring(i, j));
             }
         }
         System.out.println(set.size());
