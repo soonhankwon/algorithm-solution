c9d5412f3882a2f9fe02fea0530809ccd75c8913
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-19 21:41:16 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 668 ms, Memory: 54068 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -3,21 +3,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Map<String, Integer> map = new TreeMap<>(Comparator.reverseOrder());
+        Map<String, Boolean> map = new TreeMap<>(Comparator.reverseOrder());
         for (int i = 0; i < n; i++) {
             map.compute(br.readLine().split(" ")[0],
-                    (k, v) -> v == null ? 1 : ++v);
+                    (k, v) -> v == null || !v);
         }
         StringBuilder sb = new StringBuilder();
         map.entrySet()
                 .stream()
-                .filter(e -> e.getValue() % 2 == 1)
+                .filter(Entry::getValue)
                 .forEach(e -> sb.append(e.getKey()).append("\n"));
         System.out.println(sb);
         br.close();
