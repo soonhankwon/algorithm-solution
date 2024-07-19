4eb18357418895626afe6b56977ede3a35e8c931
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-19 21:49:49 +0900

    [Silver III] Title: 파일 정리, Time: 648 ms, Memory: 56008 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,20 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        TreeMap<String, Integer> map = new TreeMap<>();
+        Map<String, Integer> map = new TreeMap<>();
         for (int i = 0; i < n; i++) {
-            map.compute(br.readLine().split("\\.")[1],
+            String str = br.readLine();
+            map.compute(str.substring(str.lastIndexOf('.') + 1),
                     (k, v) -> v == null ? 1 : ++v);
         }
         StringBuilder sb = new StringBuilder();
-        map.forEach((key, value) -> sb.append(key).append(" ").append(value).append("\n"));
+        map.forEach((k, v) -> sb.append(k).append(" ").append(v).append("\n"));
         System.out.println(sb);
-        br.close();
     }
 }
