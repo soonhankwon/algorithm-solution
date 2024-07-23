d95998e7039db4160e9fd990f27c78fcd8fdf651
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-23 22:16:34 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 148 ms, Memory: 14940 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -6,16 +6,15 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Main {
+    static List<Integer> arr = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] inputs = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int n = inputs[0];
         int k = inputs[1];
-
-        List<Integer> arr = new ArrayList<>();
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= inputs[0]; i++) {
             arr.add(i);
         }
 
