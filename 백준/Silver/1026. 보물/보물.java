d654cb69f3c48ff8b18cbf4eb107192b811cdf2f
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-26 15:49:56 +0900

    [Silver IV] Title: 보물, Time: 120 ms, Memory: 14552 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.java
+++ 백준/Silver/1026. 보물/보물.java
@@ -2,34 +2,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.stream.IntStream;
+import java.util.Collections;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        // 길이가 N인 정수 배열 A, B
-        int[] a = new int[n];
-        int[] b = new int[n];
-
-        String[] aInputs = br.readLine().split(" ");
-        String[] bInputs = br.readLine().split(" ");
-        br.close();
-        IntStream.range(0, n).forEach(i -> {
-            a[i] = Integer.parseInt(aInputs[i]);
-            b[i] = Integer.parseInt(bInputs[i]);
-        });
-      
-        Arrays.sort(a);
-        int[] reverseSortedB = IntStream.of(b)
-                .boxed()
-                .sorted(Comparator.reverseOrder())
-                .mapToInt(Integer::intValue)
+        int[] a = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
                 .toArray();
+        Integer[] b = Arrays.stream(br.readLine().split(" "))
+                .map(Integer::parseInt)
+                .toArray(Integer[]::new);
 
-        int answer = IntStream.range(0, n).map(i -> (a[i] * reverseSortedB[i])).sum();
-        System.out.println(answer);
+        Arrays.sort(a);
+        Arrays.sort(b, Collections.reverseOrder());
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += a[i] * b[i];
+        }
+        System.out.println(sum);
+        br.close();
     }
 }
