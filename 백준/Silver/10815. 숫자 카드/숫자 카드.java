a19e0e4ede25e0ddbea9092323cabb363677f255
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-07-29 16:26:03 +0900

    [Silver V] Title: 숫자 카드, Time: 788 ms, Memory: 133252 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,39 +1,24 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        boolean[] hasNumber = new boolean[20_000_001];
+        String[] input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            hasNumber[Integer.parseInt(input[i]) + 10_000_000] = true;
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int n = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		HashMap<Integer, Integer> map = new HashMap<>();
-		for (int i = 0; i < n; i++) {
-			int x = Integer.parseInt(st.nextToken());
-			map.put(x, map.getOrDefault(x, 0) + 1);
-		}
-		
-		int m = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine());
-		
-		for (int i = 0; i < m; i++) {
-			int y = Integer.parseInt(st.nextToken());
-			if (map.get(y) == null) {
-				bw.write(0 + "\n");
-			} else {
-				bw.write(1 + "\n");
-			}
-		}
-		bw.flush();
-		bw.close();
-	}
-
+        int m = Integer.parseInt(br.readLine());
+        input = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            sb.append(hasNumber[Integer.parseInt(input[i]) + 10_000_000] ? "1 " : "0 ");
+        }
+        System.out.println(sb);
+        br.close();
+    }
 }
