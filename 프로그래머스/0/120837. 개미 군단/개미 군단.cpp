09866d2ae14950c166b23a391186b04c68299d7d
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-09-25 20:22:27 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    if(hp %  5 >= 0) {
+        answer += hp / 5;
+        hp = hp % 5;
+    }
+    if(hp % 3 >= 0) {
+        answer += hp / 3;
+        hp = hp % 3;
+    }
+    return hp == 0 ? answer : answer + hp;
+}
