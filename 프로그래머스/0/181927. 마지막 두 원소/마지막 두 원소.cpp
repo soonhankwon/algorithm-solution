fdb2d8e920207870480e5f89081a9b009f8bbae0
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-10-15 13:50:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    int size = num_list.size();
+    if(num_list[size - 1] > num_list[size - 2]) {
+        num_list.push_back(num_list[size - 1] - num_list[size - 2]);
+    } else {
+        num_list.push_back(num_list[size - 1] * 2);   
+    }
+    return num_list;
+}
