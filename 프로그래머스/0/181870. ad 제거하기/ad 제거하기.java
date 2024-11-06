e91f8782b6d6fcc4aa76052ee34ae104a9c31b5b
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-11-06 14:39:06 +0900

    [level 0] Title: ad 제거하기, Time: 3.38 ms, Memory: 94 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+            .filter(str -> !str.contains("ad"))
+            .toArray(String[]::new);
+    }
+}
