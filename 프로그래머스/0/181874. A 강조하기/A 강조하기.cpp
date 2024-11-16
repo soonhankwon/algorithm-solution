871af1ef0735e3ead620b964e4986ee1834801a5
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-11-16 13:58:49 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    string answer = "";
+    for(char c : myString) {
+        if(c == 'a' || c == 'A') {
+            answer += 'A';
+        } else if(c >= 'A' && c <= 'Z') {
+            answer += c + 32;
+        } else {
+            answer += c;
+        }
+    }
+    return answer;
+}
