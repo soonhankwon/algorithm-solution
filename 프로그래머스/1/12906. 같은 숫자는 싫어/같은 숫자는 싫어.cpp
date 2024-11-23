fef71ff83a6cbbc5ae9e26afb40a1dd445fa2878
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2024-11-24 06:13:49 +0900

    Update 같은 숫자는 싫어.cpp

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -7,7 +7,6 @@ vector<int> solution(vector<int> arr)
 {
     vector<int> answer;
     answer.push_back(arr[0]);
-    int top = 0;
     for(int i = 1; i < arr.size(); i++) {
         if(answer.back() != arr[i]) {
             answer.push_back(arr[i]);
