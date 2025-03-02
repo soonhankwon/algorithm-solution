399ff69d574844661f3aa4498deed925169abf93
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2025-03-03 01:44:35 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -6,36 +6,19 @@ import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //누적합 + 그리디(맨앞에 가장 시간이 적게 걸리는 사람이 와야함)
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[] prefixSum = new int[n];
-        int[] inputs = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        
-        //삽입시 시간복잡도 logN
+        br.readLine();
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            pq.add(inputs[i]);
-        }
-
-        int index = 0;
-        int cnt = 0;
+        Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .forEach(pq::add);
+        int time = 0;
+        int answer = 0;
         while (!pq.isEmpty()) {
-            int poll = pq.poll();
-            if (index == 0) {
-                prefixSum[index] = poll;
-                cnt += poll;
-                index++;
-                continue;
-            }
-            prefixSum[index] = prefixSum[index - 1] + poll;
-            cnt += prefixSum[index];
-            index++;
+            time = pq.poll() + time;
+            answer += time;
         }
-        System.out.println(cnt);
+        System.out.println(answer);
         br.close();
     }
 }
