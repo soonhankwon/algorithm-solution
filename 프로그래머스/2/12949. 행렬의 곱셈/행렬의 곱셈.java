a3c1978c9dbca73ac538dc68ce9a0aaf1d7ca859
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2025-04-07 11:43:09 +0900

    [level 2] Title: 행렬의 곱셈, Time: 7.04 ms, Memory: 92.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        for(int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                int num = 0;
+                for(int k = 0; k < arr1[0].length; k++) {
+                    num += arr1[i][k] * arr2[k][j];
+                }
+                answer[i][j] = num;
+            }
+        }
+        return answer;
+    }
+}
