14086a775cecb207875101d50de1760636de4397
Soonhan Kwon <113872320+soonhankwon@users.noreply.github.com>
2026-03-07 10:31:07 +0900

    [Silver IV] Title: 2+1 세일, Time: 320 ms, Memory: 27456 KB -BaekjoonHub

--- 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.java
@@ -1,28 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
     /*
      * 해결법: Greedy 알고리즘.
-     * 1. 내림차순으로 정렬
-     * 2. 인덱스가 3으로 떨어질 경우 sum에서 제외한다.
+     * 1. 오름차순으로 정렬
+     * 2. 배열의 뒤에서부터 탐색(내림차순)
+     * 3. 인덱스가 3으로 떨어질 경우 sum에서 제외한다.
      */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> products = new ArrayList<>();
+
+        int[] products = new int[n];
         for (int i = 0; i < n; i++) {
-            products.add(Integer.valueOf(br.readLine()));
+            products[i] = Integer.parseInt(br.readLine());
         }
-        products.sort((a, b) -> b - a);
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0) continue;
-            Integer product = products.get(i - 1);
-            sum += product;
+
+        Arrays.sort(products);
+
+        long sum = 0;
+        int count = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (count % 3 != 0) {
+                sum += products[i];
+            }
+            count++;
         }
         System.out.println(sum);
         br.close();
